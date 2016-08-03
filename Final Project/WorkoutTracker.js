63fc9070f46e95579aee6ed4a554c74054796e77
guox1 <Jacky@Xiluns-MacBook-Air.local>
2016-08-02 23:54:45 -0700

    1

--- Final Project/WorkoutTracker.js
+++ Final Project/WorkoutTracker.js
@@ -123,5 +123,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://52.35.2.29:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://52.39.88.179:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
